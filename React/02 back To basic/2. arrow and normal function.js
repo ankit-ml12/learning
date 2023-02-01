651852028e12be1dc330ee1c18610160d6a25ea6
ankit patidar <patiankit6555@gmail.com>
2023-02-01 16:03:53 +0530

    basic 1

---
+++ React/02 back To basic/2. arrow and normal function.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// function Greeting() {
+//   //here we have to return the jsx not html
+//   return (
+//     <div>
+//       <h1>Hello ankit Patidar</h1>;
+//     </div>
+//   );
+// }
+
+//ARROW FUNCTION
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "hello Ankit");
+// };
+
+//or
+
+const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "hello 2 type")
+  );
+};
+
+//three element are {html element, prop, content}
+// so this arrow function get messier after sometime
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
---
+++ React/02 back To basic/3. jsx rule.js
@@ -0,0 +1,10 @@
+// jsx rule :-
+`
+1. return single element  
+-->{rape everyting in div tag, div is not good if possible try to use sementic tag}
+2. if you don't want to use div you can use React.Fragment which does not create div element 
+3. use camelcase for html attribute 
+4. close every element 
+5. className instead of class
+6. formatting
+`;
