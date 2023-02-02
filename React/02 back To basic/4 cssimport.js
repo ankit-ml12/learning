2c6e21c132704cdf144dec039af616db370cc7ed
ankit patidar <patiankit6555@gmail.com>
2023-02-02 16:04:00 +0530

    js and css in jsz

--- React/02 back To basic/4 cssimport.js
+++ React/02 back To basic/4 cssimport.js
@@ -1,2 +1,6 @@
 "to import css we simply create a index.css in src folder & import it";
 import "./index.css";
+
+//if you want to use inline css inside the index.js file you can like this
+style={{ fontsize: "10px", marginTop: "10px", marginBottom: "10px" }}
+
---
+++ React/02 back To basic/5. jsInJsx.js
@@ -0,0 +1,6 @@
+//if you want to use javascript in jsx
+// you can write only which statement which return something
+const auth = "ankit patidar";
+const Authore = () => {
+  return <h4>{auth.toUpperCase()}</h4>;
+};
