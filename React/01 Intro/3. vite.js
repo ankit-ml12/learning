81d9cc53a0a663d4e012534c2f907b3ab7431f9e
ankit patidar <patiankit6555@gmail.com>
2023-02-09 14:17:19 +0530

    updates

---
+++ React/01 Intro/3. vite.js
@@ -0,0 +1,20 @@
+;`Vite Docs
+(Vite)[https://vitejs.dev/]
+
+Vite Install
+npm create vite@latest app-name -- --template react
+npm install
+npm run dev
+http://localhost:5173/
+Vite Setup
+need to use .jsx extension
+
+index.html in the source instead of public
+
+assets still in public
+
+instead of index.js, need to use main.jsx
+
+to spin up dev server - "npm run dev"
+
+rest the same - imports/exports, deployment, assets, etc...`
--- React/02 back To basic/11.0 import&export.js
+++ React/02 back To basic/11.0 import&export.js
@@ -1,25 +1,33 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "./index.css";
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
 
 //import js file
 //name must be same
-import { books } from "./11.2 books";
+import { books } from './11.2 books'
 //in case of default export you have to import like this
 // in this case you can rename of the import file
-import Book from "./11.1 book";
+import Book from './11.1 book'
 
 function Booklist() {
   return (
     <div className="booko">
       <section className="booklist">
         {books.map((book) => {
-          const { auth, title, image } = book;
-          return <Book key={book.id} book={book}></Book>;
+          const { auth, title, image } = book
+          return <Book key={book.id} book={book}></Book>
         })}
       </section>
     </div>
-  );
+  )
 }
 
-ReactDom.render(<Booklist />, document.getElementById("root"));
+ReactDom.render(<Booklist />, document.getElementById('root'))
+;`
+some time you may see this file
+<React.StrictMode>
+<APP/>
+</React.StrictMode>
+``the use of this strictMode is to find highly potential problem inside the application, it activate extra check for its decadence it only run in development it will not work or impact production build 
+
+In development it run twice `
